1ba6b6b16b287cb95e0f4cef78828217fc427a77
Alan Hayward <alan.hayward@arm.com>
2018-05-08 16:55:56 +0100

    Split warscrolls yaml from ghb2017

--- armiesofsigmar/configloader.py
+++ armiesofsigmar/configloader.py
@@ -9,6 +9,7 @@ DEFAULT_ARMY_SIZE="vanguard"
 
 def load_units(rulebook=RULEBOOK_LATEST, unitlists=["all"]):
     ret = []
+    retdict = {}
     for faction in unitlists:
         filename = os.path.join(SELF_DIR, "units", "{}_{}.yaml".format(rulebook, faction.replace(" ", "_")))
         with open(filename, 'r') as f:
@@ -17,7 +18,24 @@ def load_units(rulebook=RULEBOOK_LATEST, unitlists=["all"]):
                 if type(section) is str:
                     ret = ret + load_units(rulebook, [sectionname])
                 else:
-                    ret = ret + section
+                    filenamew = os.path.join(SELF_DIR, "units", "warscrolls_{}.yaml".format(sectionname.replace(" ", "_")))
+                    with open(filenamew, 'r') as fw:
+                        fbook = yaml.load(fw)
+                        fsection = fbook[sectionname]
+                        for unit in section:
+                            # print unit
+                            for funit in fsection:
+                                if funit["name"] == unit["name"]:
+                                    # print funit["name"]
+                                    unit.update(funit)
+                        ret = ret + section
+                        #             print section
+                        # print fsection
+                        # print section
+                        # for fsectionname, fsection in fbook.iteritems():
+                        #     print fsection
+                        #     print section
+    # print ret
     return ret
 
 def load_restictions(filename):
@@ -28,3 +46,16 @@ def load_rules(rulebook=RULEBOOK_LATEST, size=DEFAULT_ARMY_SIZE):
     filename = os.path.join(SELF_DIR, "rules", "{}_{}.yaml".format(rulebook, size))
     with open(filename, 'r') as f:
         return yaml.load(f)
+
+def load_warscrolls(unitlists=["all"]):
+    ret = []
+    for faction in unitlists:
+        filename = os.path.join(SELF_DIR, "units", "warscrolls_{}.yaml".format(faction.replace(" ", "_")))
+        with open(filename, 'r') as f:
+            book = yaml.load(f)
+            for sectionname, section in book.iteritems():
+                if type(section) is str:
+                    ret = ret + load_warascolls(rulebook, [sectionname])
+                else:
+                    ret = ret + section
+    return ret
